49bd25ae64145459f98055d5f468c24b40620928
An Hoàng Minh <121307832+hanami2002@users.noreply.github.com>
2023-10-08 23:12:12 +0700

    fix lai lan 1

--- IMS_BE/src/main/java/com/example/IMS_BE/controller/IssueController.java
+++ IMS_BE/src/main/java/com/example/IMS_BE/controller/IssueController.java
@@ -1,4 +1,23 @@
 package com.example.IMS_BE.controller;
 
+import com.example.IMS_BE.entity.Issue;
+import com.example.IMS_BE.service.IssueService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/issue")
 public class IssueController {
+    @Autowired
+    private IssueService issueService;
+    @RequestMapping("/student")
+    public List<Issue> getListIssue(){
+        return issueService.getAllIssue();
+    }
+
+
 }
