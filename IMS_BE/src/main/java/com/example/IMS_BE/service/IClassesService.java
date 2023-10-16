8fce387974525d10a0f508c8cb67637a8ebe8b79
minh3ehqz <minh3ehqz@gmail.com>
2023-10-16 09:12:45 +0700

    class manage list

--- IMS_BE/src/main/java/com/example/IMS_BE/service/IClassesService.java
+++ IMS_BE/src/main/java/com/example/IMS_BE/service/IClassesService.java
@@ -4,12 +4,14 @@ import com.example.IMS_BE.entity.Classes;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+@Service
 public interface IClassesService {
-
-        List<Classes> getAllClasses();
-       Classes insertnewClass(Classes newClass);
-
-        Classes getClassById(Integer id);
-        void deleteById(Integer id);
-
+    public List<Classes> GetAllClasses();
+    public List<Classes> GetClassesById(long id);
+    public void AddClass(Classes classes);
+    public void DeleteClass(Classes classes);
+    public void UpdateClass(Classes classes);
+    public Classes GetClassById(long id);
+    public void CancelClass(long id);
 }
